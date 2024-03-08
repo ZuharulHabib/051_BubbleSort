e142f1180b60d67c6787b3954db8a9d34d24870f
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-03-08 15:24:56 +0700

    komit 2

--- 051_BubbleSort/051_BubbleSort.cpp
+++ 051_BubbleSort/051_BubbleSort.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int arr[20]; 
+
+int arr[20];
 int n;
+
+
+void input() {
+	while (true) {
+		cout << "Masukan banyuaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke=" << (i + 1) << "; ";
+		cin >> arr[i];
+	}
+}
+
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
+}
+
+void bubbleSortArray() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+
+int main() {
+	input();
+	bubbleSortArray();
+	display();
+	return 0;
+}
